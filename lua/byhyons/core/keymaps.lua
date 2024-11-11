5498c59b95c42853f5d4b0fb44471bcc2604152c
byHYONS <byhyons@gmail.com>
2024-11-11 13:33:15 +0100

    feat: aggiunto lspsaga per suggerimenti e ottimizzazione UI

--- lua/byhyons/core/keymaps.lua
+++ lua/byhyons/core/keymaps.lua
@@ -21,3 +21,27 @@ keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" 
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tabrent buffer to new tab
+
+-- Keybindings per lspsaga
+-- Definisci le keybindings di lspsaga sotto <leader>i
+keymap.set("n", "<leader>if", "<cmd>Lspsaga lsp_finder<CR>", { desc = "LSP Finder" }) -- LSP Finder
+keymap.set("n", "<leader>id", "<cmd>Lspsaga peek_definition<CR>", { desc = "Peek Definition" }) -- Peek Definition
+keymap.set("n", "<leader>it", "<cmd>Lspsaga peek_type_definition<CR>", { desc = "Peek Type Definition" }) -- Peek Type Definition
+keymap.set("n", "<leader>ih", "<cmd>Lspsaga hover_doc<CR>", { desc = "Hover Documentation" }) -- Hover Documentation
+keymap.set("n", "<leader>ic", "<cmd>Lspsaga code_action<CR>", { desc = "Code Action" }) -- Code Action
+keymap.set("n", "<leader>ir", "<cmd>Lspsaga rename<CR>", { desc = "Rename" }) -- Rename
+keymap.set("n", "<leader>iR", "<cmd>Lspsaga rename ++project<CR>", { desc = "Rename Project" }) -- Rename Project
+keymap.set("n", "<leader>il", "<cmd>Lspsaga outline<CR>", { desc = "Outline" }) -- Outline
+
+-- Aggiungi le nuove keybindings per la diagnostica
+keymap.set("n", "<leader>is", "<cmd>Lspsaga show_cursor_diagnostics<CR>", { desc = "Show Cursor Diagnostics" }) -- Show Cursor Diagnostics
+keymap.set("n", "<leader>in", "<cmd>Lspsaga diagnostic_jump_next<CR>", { desc = "Jump to Next Diagnostic" }) -- Jump to Next Diagnostic
+keymap.set("n", "<leader>ip", "<cmd>Lspsaga diagnostic_jump_prev<CR>", { desc = "Jump to Previous Diagnostic" }) -- Jump to Previous Diagnostic
+
+-- Aggiungi le keybindings per la call hierarchy
+keymap.set("n", "<leader>ii", "<cmd>Lspsaga incoming_calls<CR>", { desc = "Incoming Calls" }) -- Incoming Calls
+keymap.set("n", "<leader>io", "<cmd>Lspsaga outgoing_calls<CR>", { desc = "Outgoing Calls" }) -- Outgoing Calls
+
+-- Aggiungi le keybindings per la navigazione nelle finestre di anteprima
+keymap.set("n", "<C-e>", "<cmd>Lspsaga scroll_down<CR>", { desc = "Scroll Down" }) -- Scroll Down
+keymap.set("n", "<C-y>", "<cmd>Lspsaga scroll_up<CR>", { desc = "Scroll Up" }) -- Scroll Up
---
+++ lua/byhyons/plugins/lsp/lspsaga.lua
@@ -0,0 +1,54 @@
+return {
+  "nvimdev/lspsaga.nvim",
+  event = "LspAttach", -- Carica il plugin quando un server LSP si attacca
+  dependencies = {
+    "nvim-tree/nvim-web-devicons", -- Dipendenza per le icone
+    "nvim-treesitter/nvim-treesitter", -- Dipendenza per l'analisi del codice
+  },
+  config = function()
+    require("lspsaga").setup({
+      -- Configurazione UI
+      ui = {
+        border = "rounded", -- Bordo delle finestre di lspsaga
+        winblend = 0, -- Trasparenza delle finestre
+        expand = "", -- Icona per espandere
+        collapse = "", -- Icona per comprimere
+        preview = " ", -- Icona per l'anteprima
+        code_action = "💡", -- Icona per le azioni di codice
+        diagnostic = "🐛", -- Icona per le diagnostiche
+        incoming = " ", -- Icona per i cambiamenti in entrata
+        outgoing = " ", -- Icona per i cambiamenti in uscita
+        hover = " ", -- Icona per il hover
+        kind = {}, -- Puoi personalizzare ulteriormente i tipi di simboli
+        diagnostic_head = "🐛 ",
+        diagnostic_sign = " ",
+      },
+      -- Abilita il Lightbulb per le azioni rapide
+      lightbulb = {
+        enable = true,
+        sign = true,
+        sign_priority = 20,
+        virtual_text = false,
+      },
+      -- Configurazione della Winbar per i simboli
+      symbol_in_winbar = {
+        enable = true,
+        separator = " > ",
+        hide_keyword = true,
+        show_file = true,
+        folder_level = 2,
+        respect_root = false,
+        color_mode = true,
+      },
+      -- Abilita la navigazione call hierarchy
+      callhierarchy = {
+        enable = true,
+      },
+      -- Abilita le funzionalità di navigazione diagnostica
+      diagnostic = {
+        show_code_action = true,
+      },
+      -- Altre opzioni di configurazione possono essere aggiunte qui
+    })
+  end,
+}
