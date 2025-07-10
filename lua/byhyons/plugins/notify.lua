9432d83ee58bdf944fcdde53ce3a928986181b82
byHYONS <byhyons@gmail.com>
2025-07-10 19:15:49 +0200

    fix: fixato problema in notify

--- lua/byhyons/plugins/notify.lua
+++ lua/byhyons/plugins/notify.lua
@@ -1,24 +1,29 @@
+-- NOTIFICHE DI NVIM
+
 return {
   "rcarriga/nvim-notify",
-  config = function()
-    require("notify").setup({
-      stages = "fade_in_slide_out",
-      timeout = 1500,
-      level = "info", -- Livello di notifica di default (info, warn, error)
-      max_width = 50, -- Larghezza massima della notifica
-      max_height = 10, -- Altezza massima della notifica
-      background_colour = "#011423", -- Colore di sfondo per la trasparenza
-      icons = {
-        ERROR = "",
-        WARN = "",
-        INFO = "",
-        DEBUG = "",
-        TRACE = "✎",
-      },
-      time_formats = {}, -- Lascia vuoto per usare il formato di default
-      on_open = nil, -- Funzione opzionale da eseguire all'apertura
-      on_close = nil, -- Funzione opzionale da eseguire alla chiusura
-    })
+  -- tag = "v3.15.0",
+  version = "*",
+  opts = {
+    stages = "fade_in_slide_out",
+    timeout = 1500,
+    level = "info", -- Livello di notifica di default (info, warn, error)
+    max_width = 50, -- Larghezza massima della notifica
+    max_height = 10, -- Altezza massima della notifica
+    background_colour = "#011423", -- Colore di sfondo per la trasparenza
+    icons = {
+      ERROR = "",
+      WARN = "",
+      INFO = "",
+      DEBUG = "",
+      TRACE = "✎",
+    },
+    time_formats = {}, -- Lascia vuoto per usare il formato di default
+    on_open = nil, -- Funzione opzionale da eseguire all'apertura
+    on_close = nil, -- Funzione opzionale da eseguire alla chiusura
+  },
+  config = function(_, opts)
+    require("notify").setup(opts)
     vim.notify = require("notify")
   end,
 }
