37f75ff29796a4a31c8c468379cca865e6e9489d
byHYONS <byhyons@gmail.com>
2025-07-21 06:16:57 +0200

    Feat: aggiunto al progetto laravel.nvim
    prezioso tool per comandi artisan e per mappa rotte progetto

---
+++ lua/byhyons/plugins/laravel.lua
@@ -0,0 +1,13 @@
+-- TOOL LARAVEL
+return {
+  "adalessa/laravel.nvim",
+  dependencies = {
+    "tpope/vim-dotenv",
+    "nvim-telescope/telescope.nvim",
+    "MunifTanjim/nui.nvim",
+    "kevinhwang91/promise-async",
+  },
+  cmd = { "Laravel" },
+  event = { "VeryLazy" },
+  config = true,
+}
