cf4739ffc1a646ddeabc9fdcef2cbd5881a70a40
byHYONS <byhyons@gmail.com>
2025-11-15 15:12:08 +0100

    fix: corretto limite caratteri nei file python, per evitare warning

--- lua/custom/configs/lint.lua
+++ lua/custom/configs/lint.lua
@@ -23,3 +23,5 @@ vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
 vim.keymap.set("n", "<leader>ll", function()
   lint.try_lint()
 end, { desc = "Trigger linting for current file" })
+
+lint.linters.flake8.args = { "--max-line-length=150" }
